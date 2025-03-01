58d0063176648b03cd01ccfe2f6797c41c860673
github-action-benchmark <github@users.noreply.github.com>
2025-03-01 15:14:44 +0000

    add Rust Benchmark (cargo) benchmark result for a2a66efb207b6854adb17839cf945375aced8dae

---
+++ docs/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1740842084338,
+  "repoUrl": "https://github.com/skanehira/rust-cli-template",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "committer": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "distinct": true,
+          "id": "a2a66efb207b6854adb17839cf945375aced8dae",
+          "message": "chore: benchmark-data-dir-path",
+          "timestamp": "2025-03-02T00:14:09+09:00",
+          "tree_id": "dc08b3040bd633fb8b1af1e4e6caaf7825be325c",
+          "url": "https://github.com/skanehira/rust-cli-template/commit/a2a66efb207b6854adb17839cf945375aced8dae"
+        },
+        "date": 1740842083479,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 18.02,
+            "range": "± 0.09",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
