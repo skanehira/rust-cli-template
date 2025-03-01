a4841e08bc88323122586d5de70b9e8ad1b82579
github-action-benchmark <github@users.noreply.github.com>
2025-03-01 15:18:46 +0000

    add Rust Benchmark (cargo) benchmark result for e8f56a66b800c0d5a39a3f414089a2d72d65cc4f

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740842084338,
+  "lastUpdate": 1740842326800,
   "repoUrl": "https://github.com/skanehira/rust-cli-template",
   "entries": {
     "Rust Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "tests::bench_fib",
             "value": 18.02,
             "range": "± 0.09",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "committer": {
+            "email": "sho19921005@gmail.com",
+            "name": "skanehira",
+            "username": "skanehira"
+          },
+          "distinct": true,
+          "id": "e8f56a66b800c0d5a39a3f414089a2d72d65cc4f",
+          "message": "chore: add LICENSE",
+          "timestamp": "2025-03-02T00:18:17+09:00",
+          "tree_id": "c0ea65cb3a41c33f19918ec12b27e28426558748",
+          "url": "https://github.com/skanehira/rust-cli-template/commit/e8f56a66b800c0d5a39a3f414089a2d72d65cc4f"
+        },
+        "date": 1740842326563,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 18.02,
+            "range": "± 0.18",
             "unit": "ns/iter"
           }
         ]
