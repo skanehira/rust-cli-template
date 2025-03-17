3671f9837b1a6f7a7af4827a0018ad552e55fa34
github-action-benchmark <github@users.noreply.github.com>
2025-03-17 01:12:34 +0000

    add Rust Benchmark (cargo) benchmark result for 82056f34455499c3fe5e46b075ce2e630dd7ec90

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740842377573,
+  "lastUpdate": 1742173954750,
   "repoUrl": "https://github.com/skanehira/rust-cli-template",
   "entries": {
     "Rust Benchmark": [
@@ -89,6 +89,36 @@ window.BENCHMARK_DATA = {
             "name": "tests::bench_fib",
             "value": 18.02,
             "range": "± 0.10",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "82056f34455499c3fe5e46b075ce2e630dd7ec90",
+          "message": "build(deps): Bump clap from 4.5.31 to 4.5.32 (#4)\n\nBumps [clap](https://github.com/clap-rs/clap) from 4.5.31 to 4.5.32.\n- [Release notes](https://github.com/clap-rs/clap/releases)\n- [Changelog](https://github.com/clap-rs/clap/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/clap-rs/clap/compare/v4.5.31...clap_complete-v4.5.32)\n\n---\nupdated-dependencies:\n- dependency-name: clap\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-03-17T10:11:49+09:00",
+          "tree_id": "d57ac3b49345154987ed0c2dc6995a4ad3257283",
+          "url": "https://github.com/skanehira/rust-cli-template/commit/82056f34455499c3fe5e46b075ce2e630dd7ec90"
+        },
+        "date": 1742173953721,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 18.01,
+            "range": "± 0.09",
             "unit": "ns/iter"
           }
         ]
