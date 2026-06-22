f94f8b6d1b485b616c5ac7ac301b75f5c6400465
github-action-benchmark <github@users.noreply.github.com>
2026-06-22 00:39:39 +0000

    add Rust Benchmark (cargo) benchmark result for b490b8ae0997fb98ccd891ce1fa3732ab5dbee5f

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1780923164879,
+  "lastUpdate": 1782088779920,
   "repoUrl": "https://github.com/skanehira/rust-cli-template",
   "entries": {
     "Rust Benchmark": [
@@ -839,6 +839,36 @@ window.BENCHMARK_DATA = {
             "name": "tests::bench_fib",
             "value": 19,
             "range": "± 1.04",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b490b8ae0997fb98ccd891ce1fa3732ab5dbee5f",
+          "message": "build(deps): Bump actions/checkout from 6.0.3 to 7.0.0 (#83)\n\nBumps [actions/checkout](https://github.com/actions/checkout) from 6.0.3 to 7.0.0.\n- [Release notes](https://github.com/actions/checkout/releases)\n- [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/actions/checkout/compare/df4cb1c069e1874edd31b4311f1884172cec0e10...9c091bb21b7c1c1d1991bb908d89e4e9dddfe3e0)\n\n---\nupdated-dependencies:\n- dependency-name: actions/checkout\n  dependency-version: 7.0.0\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-06-22T09:39:14+09:00",
+          "tree_id": "9778233bdf6d6085a4924c9f5674cd12e01a8fb5",
+          "url": "https://github.com/skanehira/rust-cli-template/commit/b490b8ae0997fb98ccd891ce1fa3732ab5dbee5f"
+        },
+        "date": 1782088779086,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "tests::bench_fib",
+            "value": 18.95,
+            "range": "± 2.25",
             "unit": "ns/iter"
           }
         ]
